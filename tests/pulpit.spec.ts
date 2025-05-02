8e2976a5baa74b00f4d2d3a0aba5316fd2d17cb6
Hubert Kieruczenko <hub.kieruczenko@gmail.com>
2025-05-02 12:30:41 +0200

    commit

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -23,4 +23,23 @@ test.describe('Pulpit tests', () => {
     );
   });
 
-});
+  test.only('successful mobile top-up', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('tester12');
+    await page.getByTestId('password-input').fill('password');
+    await page.getByTestId('login-button').click();
+
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+    await page.locator('#widget_1_topup_amount').fill('50');
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+
+    await expect(page.locator('#show_messages')).toHaveText(
+      'Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx'
+    );
+  });
+}); 
