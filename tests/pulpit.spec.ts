13792183175c96f030fd63b3932d68ca9ebbc92b
Hubert Kieruczenko <hub.kieruczenko@gmail.com>
2025-05-28 08:24:53 +0200

    commit

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -6,7 +6,7 @@ test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
     const login = 'testerQQ';
     const password = 'password';
-    
+
     await page.goto('/');
     await page.getByTestId('login-input').fill(login);
     await page.getByTestId('password-input').fill(password);
@@ -51,5 +51,21 @@ test.describe('Pulpit tests', () => {
     await page.getByTestId('close-button').click();
 
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+  });
+
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '50';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 });
